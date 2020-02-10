fc2a4d797051c63ada3325f6a7d25912276e857a
Kortney <kfiel48@wgu.edu>
2020-02-10 12:33:49 -0600

    added range and invald ValueErrors

--- guessnumber.py
+++ guessnumber.py
@@ -4,26 +4,47 @@ import random
 
 guessNum = 0
 guessLimit = 3
+low = 0
+high = 20
+num = random.randint(low,high)
+
+#low = int(input("Enter low number: "))
+#high = int(input("Enter high number: "))
 
 print(" ")
 print("     ***Welcome to the Great Number Guessing Game***")
 print(" ")
 
-low = int(input("Enter low number: "))
-high = int(input("Enter high number: ")) 
-
-num = random.randint(low,high)
-
-while True:
-    guess = int(input("guess number:"  ))
-    print(" ")
-    if guess == num:
-        print("CONGRATS!! You guess the number! ")
+while guessNum < guessLimit:
+    try:
+        guess = int(input("guess number between {} and {}:  ". format(low, high)))
         print(" ")
-        break
+    except ValueError:
+        print("Not at valid number. Try again")
+        continue
+    if guess > high or guess < low:
+        print("Your guess is out of range. Please pick a number betwee {} and {}".format(low,high))
+        print(" ")
     else:
         guessNum = guessNum + 1
-        print("SORRY not it, try again")
+        if guess > num:
+            print("too high")
+        if guess < num:
+            print("too low")
         print("You have {} guesses left".format(guessNum))
         print(" ")
         pass
+
+while True:
+    var = print("Would you like to play again? (y/n):  ")
+    if var == "n" or "N":
+        break
+if guess == num:
+    print("CONGRATS!! You guess the number! ")
+    print(" ")
+
+
+if guessNum == guessLimit:
+    print("You have guessed too many times. Goodbye")
+    print("The secret number was ** {} **".format(num))
+    print(" ")
